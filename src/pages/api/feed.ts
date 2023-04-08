29e792d15cb6880b9f40803d80564ab9be14ca2f
Henrique Fornazeiro <henrique.fornazeiro@hotmail.com>
2023-04-08 15:06:13 -0300

    get user logged and get feed by user id

---
+++ src/pages/api/feed.ts
@@ -0,0 +1,45 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { standardAnswer } from "../../types/standardAnswer";
+import { validateJWTToken } from "../../middlewares/validateJWTToken";
+import { connectMongoDB } from "../../middlewares/connectMongoDB";
+import { UserModel } from "@/models/UserModel";
+import { PublicationModel } from "@/models/PublicationModel";
+
+const endpointFeed = async (
+  req: NextApiRequest,
+  res: NextApiResponse<standardAnswer | any>
+) => {
+  try {
+    if (req.method === "GET") {
+      
+      if (req.url) {
+        const url = req.url
+        const id = url.substring(13,url.length)
+                
+        const user = await UserModel.findById(id);
+        
+        if (!user) {
+          return res.status(400).json({ error: "User not found" });
+        }
+
+        const publications = await PublicationModel.find({
+          userId: user._id,
+        }).sort({ date: -1 });
+       
+        return res.status(200).json(publications);
+
+      }
+
+      return res.status(400).json({ error: "Could not get user data" });
+    }
+
+    return res.status(405).json({ error: "Method not allowed" });
+
+  } catch (error) {
+    console.log(error);
+  }
+
+  return res.status(400).json({ error: "Could not get feed data" });
+};
+
+export default validateJWTToken(connectMongoDB(endpointFeed));
--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -1,10 +1,25 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import {validateJWTToken} from "../../middlewares/validateJWTToken";
+import type { standardAnswer } from "../../types/standardAnswer";
+import { validateJWTToken } from "../../middlewares/validateJWTToken";
+import { connectMongoDB } from "../../middlewares/connectMongoDB";
+import { UserModel } from "@/models/UserModel";
 
-const endpointUser = async (req: NextApiRequest, res: NextApiResponse) => {
-    return res.status(200).json('Usuário autenticado');
-}
+const endpointUser = async (
+  req: NextApiRequest,
+  res: NextApiResponse<standardAnswer | any>
+) => {
+  try {
+    const { userId } = req?.query;
+    const user = await UserModel.findById(userId);
 
-export default validateJWTToken(endpointUser);
+    user.password = null;
 
+    return res.status(200).json(user);
+    
+  } catch (e) {
+    console.log(e);
+    return res.status(400).json({error:"Could not get user data"});
+  }
+};
 
+export default validateJWTToken(connectMongoDB(endpointUser));
